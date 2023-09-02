4b488387c8e8340d0b09153c32cefd3fe27bacc9
Sadaf-A <sadaf20043006@gmail.com>
2023-09-02 21:50:25 +0530

    added: initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.use(express.static('public'));
+
+// Set View's
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// Navigation
+app.get('', (req, res) => {
+    res.render('index', { text: 'Hey' })
+})
+
+app.get('/about', (req, res) => {
+   res.render('about', { text: 'about'});
+})
+
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`))
