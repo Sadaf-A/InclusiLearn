0ff64f3e99d1fa65c6bfbaad4f544b243f4fae38
tuhinaww <iceybubblegumtea@gmail.com>
2023-09-03 00:11:37 +0530

    modified:   app.js 	new file:   public/css/login.css 	new file:   views/login.ejs

--- app.js
+++ app.js
@@ -18,5 +18,9 @@ app.get('/about', (req, res) => {
    res.render('about', { check: 'about', name: "new"});
 })
 
+app.get('/login', (req, res) => {
+    res.render('login', { check: 'login', name: "new"});
+ })
+
 // Listen on Port 5000
 app.listen(port, () => console.info(`App listening on port ${port}`))
