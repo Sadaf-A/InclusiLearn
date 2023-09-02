f2098e869958eaaba02f581a9ee6a26cc0701f49
Sadaf-A <sadaf20043006@gmail.com>
2023-09-03 00:11:24 +0530

    modifies: adding registration view

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // Imports
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
+
+// Use bodyParser middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -18,9 +22,15 @@ app.get('/about', (req, res) => {
    res.render('about', { text: 'about'});
 });
 
-app.post('/register', (req, res) => {
-    res.render('registeration');
+app.get('/register', (req, res) => {
+    res.render('registration');
 });
+
+app.post('/register', (req, res) => {
+    const { username, email, password } = req;
+  
+    res.send(`Registration successful for ${username}`);
+})
 
 // Listen on Port 5000
 app.listen(port, () => console.info(`App listening on port ${port}`));
