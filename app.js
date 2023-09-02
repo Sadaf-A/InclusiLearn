609584aa675c25aaa90f915289b17300cc89ff2a
Sadaf-A <sadaf20043006@gmail.com>
2023-09-02 23:41:06 +0530

    modified: modularising code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Imports
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
 app.use(express.static('public'));
 
@@ -11,12 +11,16 @@ app.set('view engine', 'ejs');
 
 // Navigation
 app.get('', (req, res) => {
-    res.render('index', { text: 'Hey' })
-})
+    res.render('index', { text: 'Hey' });
+});
 
 app.get('/about', (req, res) => {
    res.render('about', { text: 'about'});
-})
+});
+
+app.post('/register', (req, res) => {
+    res.render('registeration');
+});
 
 // Listen on Port 5000
-app.listen(port, () => console.info(`App listening on port ${port}`))
+app.listen(port, () => console.info(`App listening on port ${port}`));
