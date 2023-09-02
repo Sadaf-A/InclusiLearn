a446ceda5becfab377ca5c9b4612e42d813c7e13
Sadaf-A <sadaf20043006@gmail.com>
2023-09-03 02:26:29 +0530

    modified: removing unnessary comments

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ var server = require('http').Server(app);
 const port = 5000;
 app.use(express.static('public'));
 
-// Set View's
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// Navigation
 app.get('', (req, res) => {
     res.render('index', { text: 'Hey' });
 });
@@ -17,28 +15,7 @@ app.get('/about', (req, res) => {
    res.render('about', { text: 'about'});
 });
 
-// Load and use your routes
-const registrationRoutes = require('./routes/registrationRoutes'); // Replace with your route file(s)
-app.use('/', registrationRoutes); // Mount your routes
-
-
-// app.get('/register', (req, res) => {
-//     res.render('registration');
-// });
-
-// app.post('/register', (req, res) => { 
-//    const form = new formidable.IncomingForm();   
-//    form.parse(req, (err, fields, files) => {
-//     if (err) {
-//       console.error(err);
-//       return res.status(500).json({ error: 'Error parsing form data' });
-//     }
-
-//     // Access form fields in `fields` object
-//     const { username, password } = fields;
-
-//     res.json({ message: 'Form data received successfully', username, password });
-//   });
-// })
+const registrationRoutes = require('./routes/registrationRoutes'); 
+app.use('/', registrationRoutes); 
 
 server = app.listen(port);
--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,13 +1,10 @@
 const formidable = require('formidable');
-// registrationController.js
 const registrationService = require('../services/RegistrationService');
 
-// Handle GET request for the registration form
 exports.getRegistrationForm = (req, res) => {
   res.render('registration');
 };
 
-// Handle POST request for registration
 exports.registerUser =  (req, res) => {
     const form = new formidable.IncomingForm();   
     form.parse(req, (err, fields, files) => {
@@ -16,7 +13,6 @@ exports.registerUser =  (req, res) => {
        return res.status(500).json({ error: 'Error parsing form data' });
      }
  
-     // Access form fields in `fields` object
      const { username, password } = fields;
  
      res.json({ message: 'Form data received successfully', username, password });
