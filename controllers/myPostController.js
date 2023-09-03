ea94d38a22408eecbd5891bf1c4f5ce2f4823721
sadaf <sadaf20043006@gmail.com>
2023-09-03 18:50:52 +0530

    fixed: register, login

--- controllers/myPostController.js
+++ controllers/myPostController.js
@@ -3,5 +3,5 @@ const UserService = require('../services/UserService');
 exports.getmyPosts = async (req, res) => {
   console.log(req.session.userId);
   const username = await UserService.getUsername(req.session.userId);
-  res.render('upload', { check: 'myPost', name: username});
+  res.render('mypost', { check: 'myPost', name: username});
 };
